2d636f0665b98d170d48af6561858981af067eb0
eunjongin <eunjongin1@gmail.com>
2012-04-17 16:43:53 +0900

    First commit! Yay!

---
+++ JavaEx/src/ch1/Ex1.java
@@ -0,0 +1,13 @@
+package ch1;
+
+public class Ex1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+	}
+
+}
