6176c9b8138aa9886fc349dbf5e39b56d40a04a9
eunjongin <eunjongin1@gmail.com>
2012-04-18 13:55:19 +0900

    de

--- JavaEx/src/ch3/GuguDanEx1.java
+++ JavaEx/src/ch3/GuguDanEx1.java
@@ -8,7 +8,7 @@ public class GuguDanEx1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method
 		int dan = Integer.parseInt(args[0]);
-		System.out.println(dan + "´Ü");
+		System.out.println(dan + "´Ü222");
 		System.out.println("---------------");
 		for (int i = 1; i <= 9; i++)
 			System.out.println(dan + " * " + i + " = " + (dan * i));
